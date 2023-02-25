951420d1405884a372f112c434a15c874173f1c2
dolguin <dolguin@servel.cl>
2023-02-25 13:30:26 -0300

    feat: :fire: configuración tailwind
    Se configura tailwind y se eliminan paginas pro defecto vue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: [
+    './index.html', './src/**/*.{vue,js,ts,jsx,tsx}'
+  ],
+  darkMode: 'class', // or 'media'
   theme: {
     extend: {}
   },
