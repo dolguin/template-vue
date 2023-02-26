793c34bd73828e88eec0e8afdc59ec2725eb699f
dolguin <dolguin@servel.cl>
2023-02-26 16:09:58 -0300

    fix: :art: link sitio web
    Se agrega variable de entorno para el sitio web, enlazado al footer

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -3,6 +3,7 @@ export default {
   data: function () {
     return {
       empresa: import.meta.env.VITE_API_EMPRESA || "Nombre empresa",
+      web: import.meta.env.VITE_API_WEB || "https://www.google.com/",
     };
   },
 }
@@ -13,7 +14,9 @@ export default {
     <hr class=" bg-gray-100" />
     <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
       <div class="flex text-sm text-gray-300">
-        <p class="border-r-2 pr-4">{{ empresa }}</p>
+        <a :href="web">
+          <p class="border-r-2 pr-4">{{ empresa }}</p>
+        </a>
         <p class="pl-4">Division TI</p>
       </div>
       <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start text-xs text-gray-300">
